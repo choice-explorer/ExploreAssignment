e314a8cb350f3d7f5bd98abedbcab1745841e0af
choice-explorer <30986019+rsharma96@users.noreply.github.com>
2017-12-12 13:12:48 +0530

    updated

--- CollectionFeatures.java
+++ CollectionFeatures.java
@@ -10,6 +10,10 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import org.apache.commons.collections4.CollectionUtils;
+
+ 
+
 public class CollectionFeatures {
 
 	public static void main(String[] args) {
@@ -26,19 +30,47 @@ public class CollectionFeatures {
 		System.out.println("Before sorting " + array);
 		Collections.sort(array);
 		System.out.println("After sorting " + array);
-		
 
+		ArrayList<String> arr = new ArrayList<String>();
+		arr.add("Rama");
+		arr.add("Sushant");
+		arr.add("Andy");
+		arr.add("Akash");
+		arr.add("John");
+
+		// to validate wheather array contains all the object as we have arr
+		System.out.println(array.containsAll(arr)); // this will be false since Andy and John not present in array
+ 
+		
+		// To compare matching values between two lists
+		ArrayList<String> equalValue = new ArrayList<String>(); // this is the arraylist to store matching values
+		for (String s1 : array) {
+			for (String s2 : arr) {
+				if (s1.equals(s2)) {
+					equalValue.add(s1);
+					break;
+				}
+
+			}
+
+		}
+		System.out.println(equalValue);
+		
+		//There is another way to compare two List using 
+		//org.apache.commons.collections4.CollectionUtils library
+		// Download commons-collections from apache latest jar to the project
+		System.out.println(CollectionUtils.isEqualCollection(array, arr)); // to make sure whether lists are equal or not
+		System.out.println(CollectionUtils.intersection(array, arr)); // to find the matching objects between 2 different lists
+		System.out.println(CollectionUtils.disjunction(array, arr)); // to find the non matching objects between 2 different lists
 
 		// To sort the array in descending order
 		System.out.println("Before sorting " + array);
 		Collections.sort(array);
-		Collections.reverse(array); 
+		Collections.reverse(array);
 		// ----or---
-		Collections.sort(array,Collections.reverseOrder());
+		Collections.sort(array, Collections.reverseOrder());
 		System.out.println("After sorting " + array);
-		
-		
-		
+
 		HashSet<Integer> hs = new HashSet<Integer>();
 
 		hs.add(123);
@@ -48,7 +80,7 @@ public class CollectionFeatures {
 		hs.add(3564);
 
 		System.out.println("Before sorting " + hs);
-		//Collections.sort(hs); // cannot sort Map with Collections.sort()
+		// Collections.sort(hs); // cannot sort Map with Collections.sort()
 		System.out.println("After sorting " + hs);
 
 		HashMap<Integer, String> mp = new HashMap<Integer, String>();
@@ -58,7 +90,7 @@ public class CollectionFeatures {
 		mp.put(125, "John");
 
 		System.out.println("Before sorting " + array);
-		//Collections.sort(mp); // cannot sort Map with Collections.sort()
+		// Collections.sort(mp); // cannot sort Map with Collections.sort()
 		System.out.println("After sorting " + array);
 
 	}
