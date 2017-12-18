3c10dfb74761f743cfa889170d662d0f15978308
choice-explorer <30986019+rsharma96@users.noreply.github.com>
2017-12-18 19:11:13 +0530

    cicle without parameter added

---
+++ CircleWithoutParameter.java
@@ -0,0 +1,58 @@
+/**
+ * @author gaurnitai
+ * @created_date Sep 20, 2017
+ */
+
+package com.javabasics.assignment;
+
+/*	(Area and perimeter of a circle) Write a program that displays the area and perimeter of a circle 
+ * that has a radius of 5.5 using the following formula: 
+ * perimeter = 2 * radius * pi   ---- where pi=3.14
+ * area = radius * radius * pi ------where pi=3.14
+
+ */
+public class CircleWithoutParameter {
+
+	double radius = 5.5;
+	final static float pi = 3.14f; // its universal constant hence defined as final keyword
+
+	public void calculateArea() {
+
+		double area = radius * radius * pi;
+		System.out.printf("The area of circle having radius %.2f is %.2f\n", radius, area);
+
+		// other way to print the same
+
+		System.out.println("The area of circle having radius " + radius + " is " + area);
+
+	}
+
+	public void calculatePerimeter() {
+
+		double perimeter = 2 * radius * pi;
+		System.out.printf("The perimeter of circle having radius %.2f is %.2f\n", radius, perimeter);
+
+		// other way to print the same
+
+		System.out.println("The perimeter of circle having radius " + radius + " is " + perimeter);
+
+	}
+
+	public static void main(String[] args) {
+
+		CircleWithoutParameter c = new CircleWithoutParameter();
+		System.out.println("Area of circle using method with parameter");
+
+		System.out.println("\n");
+		System.out.println("Area of circle using method without parameter");
+		c.calculateArea();
+		System.out.println("\n");
+		System.out.println("Perimeter of circle using method with parameter");
+
+		System.out.println("\n");
+		System.out.println("Perimeter of circle using method without parameter");
+		c.calculatePerimeter();
+
+	}
+
+}
